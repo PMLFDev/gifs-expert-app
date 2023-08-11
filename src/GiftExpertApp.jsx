99c344e0c073e350b650d3dcc55b71aac37cb74d
Luis <ponceluis182@gmail.com>
2023-08-11 18:38:26 -0500

    GifsExpertApp First Commit

---
+++ src/GiftExpertApp.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { AddCategory, GifGrid } from "./components";
+
+
+
+export const GiftExpertApp = () => {
+
+   const [categories, setCategories] = useState([]);
+
+   const onAddCategory = (newCategory) =>{
+
+    if (categories.includes(newCategory)) return
+
+         setCategories([newCategory, ...categories]);
+
+
+    }
+
+  return (
+    <>
+    <h1>GiftExpertApp</h1>
+    <AddCategory onNewCategory = { onAddCategory }
+      />
+        {
+        categories.map((category) => (
+        
+        <GifGrid key={category} category = {category}/>
+        
+        ))
+        }
+
+
+    </>
+  )
+}
+
+
